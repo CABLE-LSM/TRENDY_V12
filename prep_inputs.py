3144ce11cbeaa48e6db015368c05dbef1b667dc3
Whyborn <lachlan.s.whyborn@gmail.com>
2024-06-21 23:41:03 +1000

    Revised method of handling globbed inputs

--- prep_inputs.py
+++ prep_inputs.py
@@ -14,6 +14,8 @@ then symlink that file to the fixed input file locations.
 import os
 import f90nml
 import subprocess
+import re
+from glob import glob
 
 def symlink_to_target(Target: str, LinkName: str):
     """Create a symlink at LinkName that points to Target."""
@@ -24,37 +26,43 @@ def symlink_to_target(Target: str, LinkName: str):
     # Check the return code to ensure successful completion
     proc.check_returncode()
 
-def symlink_suffix(Target: str, InputGlob: str):
+def symlink_suffix(Target: str, InputGlob: str, PathToStage: str):
     """Handle inputs that are set in the namelist with a prefix and the internals set a series of suffixes."""
     # We know that the Target will refer to a series of files, matching the given prefix.
+    print(f"Passed {Target}, {InputGlob} to symlink suffix")
     FileList = glob(Target + "*")
 
     # To build the symlinks, we need to capture the suffixes from each of the files in the list
     for File in FileList:
-        FileSuffix = re.search(Target + "(.*)", File).group(1)
-        symlink_to_target(Target + FileSuffix, re.sub("<suffix>", FileSuffix, InputGlob))
+        TargetFilePath, TargetFileName = os.path.split(Target)
+        InputFilePath, InputFileName = os.path.split(File)
+        # FileSuffix = re.search(Target + "(.*)", File).group(1)
+        # symlink_to_target(Target + FileSuffix, re.sub("<suffix>", FileSuffix, InputGlob))
+        symlink_to_target(File, os.path.join(PathToStage, InputFilePath, TargetFileName))
+        print(f"Symlinking {File} to {os.path.join(PathToStage, InputFilePath, TargetFileName)}")
 
     # Now we set the correct namelist option for the input
     return InputGlob.replace("<suffix>", "")
 
-def symlink_glob(Target: str, InputGlob: str):
+def symlink_glob(Target: str, InputGlob: str, PathToStage: str):
     """Handle inputs that are globbed in a different fashion to the suffix inputs, which at the moment is just the met inputs."""
+    print(f"Call symlink glob with {Target}, {InputGlob}")
     # Get the list of files matching the glob, replacing any <> keywords with *
     FileList = glob(re.sub("(<.*?>)", "*", Target))
 
     # For each of the files, we're just going to split the files into the path and the file, and append the file from the Target
     # to the path from the InputGlob
     for File in FileList:
-        TargetFilePath, TargetFileName = os.path.split(File)
+        TargetFilePath, TargetFileName = os.path.split(Target)
         InputFilePath, InputFileName = os.path.split(File)
-        symlink_to_target(File, os.path.join(InputFilePath, TargetFileName))
+        symlink_to_target(File, os.path.join(PathToStage, InputFilePath, TargetFileName))
 
     # Set the correct namelist option for the input
     _, TargetFileName = os.path.split(Target)
     InputFilePath, _ = os.path.split(InputGlob)
-    return os.path.join(TargetFileName, InputFilePath)
+    return os.path.join(InputFilePath, TargetFileName)
     
-def symlink_inputs(StageNamelist: dict, InputsNamelist: dict, Prefix: str):
+def symlink_inputs(StageNamelist: dict, InputsNamelist: dict, PathToStage: str):
     """Create symlinks between inputs defined in StageNamelist at the locations prescribed by InputsNamelist. Returns a copy of StageNamelist with the input entries changed as per InputsNamelist."""
 
     for Key, Value in InputsNamelist.items():
@@ -63,21 +71,21 @@ def symlink_inputs(StageNamelist: dict, InputsNamelist: dict, Prefix: str):
             if isinstance(Value, dict):
                 # If the value is a dictionary (i.e. a nested type in the namelist),
                 # recursively call the function.
-                symlink_inputs(StageNamelist[Key], Value, prefix)
+                symlink_inputs(StageNamelist[Key], Value, PathToStage)
             elif isinstance(Value, str):
                 if StageNamelist[Key]:
                     # Only perform the action if the original string is not empty
                     # Unfortunately, there are some options in the namelists which provide prefixes for a series of input/output files.
                     # This makes symlinking to them not a trivial exercise.
                     # The temporary measure for handling this will be to use keywords in the prescribed inputs namelist, denoted using <>
-                    if bool(re.search(r"(<suffix>)", Value)):
+                    if bool(re.search(r"<suffix>", Value)):
                         # The instance where we specify the suffix in the input namelist
-                        StageNamelist[Key] = symlink_suffix(StageNamelist[Key], os.path.join(Prefix, Value))
-                    elif bool(re.search(r"(<.*?>)", Value)):
+                        StageNamelist[Key] = symlink_suffix(StageNamelist[Key], Value, PathToStage)
+                    elif bool(re.search(r"<.*?>", Value)):
                         # Instances where there was a user namelist options, most likely <startdate> and <enddate>
-                        StageNamelist[Key] = symlink_glob(StageNamelist[Key], os.path.join(Prefix, Value))
+                        StageNamelist[Key] = symlink_glob(StageNamelist[Key], Value, PathToStage)
                     else:
-                        symlink_to_target(StageNamelist[Key], os.path.join(Prefix, Value))
+                        symlink_to_target(StageNamelist[Key], os.path.join(PathToStage, Value))
                         StageNamelist[Key] = Value
             else:
                 # Should only be string or dict
